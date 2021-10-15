1d015049a348bae104bf9e0006990a6703538d1f
Mexson Fernandes <robomex2020@gmail.com>
2021-10-15 17:25:19 +0530

    :sparkles: create nuxt3 base

---
+++ layouts/default.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="min-h-screen flex flex-row bg-gray-100">
+    <div class="flex flex-col w-56 bg-white overflow-hidden">
+      <div class="flex flex-wrap content-center p-4">
+        <img
+          class="h-28 w-28 object-cover rounded-full"
+          src="~/assets/images/mexson.png"
+          alt="Mexson Fernandes"
+        />
+      </div>
+      <ul class="flex flex-col py-4">
+        <li v-for="(tab, index) in tabs" :key="`tab-${index}`">
+          <NuxtLink
+            :to="tab.route"
+            class="
+              flex flex-row
+              items-center
+              h-12
+              transform
+              hover:translate-x-2
+              transition-transform
+              ease-in
+              duration-200
+              text-gray-500
+              hover:text-gray-800
+            "
+          >
+            <span
+              class="
+                inline-flex
+                items-center
+                justify-center
+                h-12
+                w-12
+                text-lg text-gray-400
+              "
+              ><i class="bx bx-home"></i
+            ></span>
+            <span class="text-sm font-medium">{{ tab.name }}</span>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+      tabs: [
+        {
+          route: "/",
+          name: "About",
+        },
+        {
+          route: "/projects",
+          name: "Projects",
+        },
+        {
+          route: "/skills",
+          name: "Skills",
+        },
+        {
+          route: "/interests",
+          name: "Interests",
+        },
+        {
+          route: "/blog",
+          name: "Blog",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-white font-semibold p-4 flex flex-col">
+    Tailwindcss Starter
+    <a
+      target="blank"
+      class="text-green-500"
+      href="https://github.com/productfrontenddeveloper/"
+      >Follow Me Github</a
+    >
+    <NuxtLink class="mt-5" to="modal">Go Modal Page</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
